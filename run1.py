7e5963b0c09226c1004c3ff05f387ac49cb266e8
zaka1 <zaka412229@pchome.com.tw>
2016-11-06 10:47:28 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 import os
 from flask import Flask
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "<h1> this is a test! </h1>"
+    return "this is a test!"
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(port=port)
